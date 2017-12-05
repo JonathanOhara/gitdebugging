62394f5c30655956ef86744918c3ddfae8a1e04f
Jonathan <jaraujo@avenuecode.com>
2017-12-05 18:20:27 -0200

    Hello World Endpoint

--- src/main/java/edu/ac/gitdebugging/App.java
+++ src/main/java/edu/ac/gitdebugging/App.java
@@ -1,13 +1,19 @@
 package edu.ac.gitdebugging;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	@RequestMapping("/sayHello")
+	public String sayHello() {
+		return "Hello";
+	}
 }
