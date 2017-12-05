5f1231c6337b79ea2509f8f740fb29230a74479a
Jonathan <jaraujo@avenuecode.com>
2017-12-05 15:39:16 -0200

    First commit

---
+++ src/main/java/edu/ac/gitdebugging/App.java
@@ -0,0 +1,13 @@
+package edu.ac.gitdebugging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
