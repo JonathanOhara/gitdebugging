8dc8f8a2bd65f53e25b7a4fe03c754fbd16fe1ac
Jonathan <jaraujo@avenuecode.com>
2018-01-12 16:24:12 -0200

    bisect step by step

---
+++ src/test/java/edu/ac/gitdebugging/web/Other.java
@@ -0,0 +1,40 @@
+package edu.ac.gitdebugging.web;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class Other {
+
+	@Autowired
+	private WebApplicationContext context;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() throws Exception{
+		mockMvc = MockMvcBuilders.webAppContextSetup(context)
+				.build();
+	}
+	
+	@Test public void 
+	shouldDeletePersonWhenDelete ()
+		throws Exception{
+		mockMvc.perform(get("/persons/3")).
+			andExpect(status().isNotFound()).
+			andReturn();
+	}
+	
+}
